592aec3a07f222db3c3bcd0276ef483861a76461
Saanvi Belede Naga <saanvibeledenaga@gmail.com>
2025-08-28 22:19:17 +0530

    Add files via upload

---
+++ Tasks1.java
@@ -0,0 +1,22 @@
+public class Tasks1
+{
+	public static void main(String[] args)
+	//Q1.Print value of sum of first 10 whole numbers.
+	{
+		int sum=0;
+		for(int i=1;i<=10;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	
+	//Q2.Print value of product of first 5 natural numbers.
+	
+		int product=1;
+		for(int i=1;i<=5;i++)
+		{
+			product=product*i;
+		}
+		System.out.println(product);
+	}
+}
